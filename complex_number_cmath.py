c7b64444fb3d8914375d0abcf028e3038caea5b6
saisrinivaskondreddy <saisrinivas.chowdary@gmail.com>
2016-10-28 19:48:14 +0300

    28-Oct-2016: Updated for negetive test case, added regular expressions

--- complex_number_cmath.py
+++ complex_number_cmath.py
@@ -5,8 +5,23 @@ import cmath
 import re
 
 complex_number=raw_input("")
+postive_img=re.compile('[0-9]*\+[0-9]*j')
+negtive_img=re.compile('[0-9]*-[0-9]*j')
+negtive_rel=re.compile('-[0-9]*\+[0-9]*j')
+negtive_both=re.compile('-[0-9]*-[0-9]*j')
 
-real,img=complex_number.split("+")
+if re.match(postive_img, complex_number):
+	real,img=complex_number.split("+")
+elif re.match(negtive_img, complex_number):
+	real,img=complex_number.split("-")
+elif re.match(negtive_rel, complex_number):
+	real,img=complex_number.split("+")
+elif re.match(negtive_both, complex_number):
+	occurance=[m.start for m in re.finder(negtive_both, complex_number)]
+	real=str[:occurance[1]]
+	img=str[occurance[1]+1:]
+
+#real,img=complex_number.split("+")
 #img_1=[int(s) in img.split() if s.isdigit()]
 img_1,dummy =img.split("j")
 
